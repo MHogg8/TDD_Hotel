a08e65f04dd505d5d5e8cdc8d3a1f1979ec2d7f6
Matthew Hogg <matthewhogg8@gmail.com>
2016-02-28 11:20:39 +0000

    added the function with that displays rooms available.

--- hotel.rb
+++ hotel.rb
@@ -7,36 +7,60 @@ class Hotel
   def initialize(hotel_size, number_of_guests)
     @num_of_rooms = hotel_size
     @num_of_guests = number_of_guests
-    @rooms_taken = []
-    @rooms_at_hotel = []
-    @rooms_free = []
+    
   end
+######################################################
+#functions for cheking-in the guest 
+######################################################
 
+def new_guest #creates a new guest
+  @guest_new = Guest.new("Tommy Amsterdam", 1, 3)
+  return @guest_new.name
+end
+
+# def push_to_guests(new_guest)
+
+# end
+
+######################################################
+#functions for the hotel rooms
+######################################################
 
 def number_of_rooms     #this returns the number of rooms in the hotel
   return @num_of_rooms.count
 end
 
-
 def number_of_rooms_booked  #this returns the number of rooms in the hotel booked
   return @num_of_guests.count
 end
 
-
-def number_of_rooms_available
+def number_of_rooms_available #num of rooms available
   number_of_rooms - number_of_rooms_booked
 end
 
+def the_rooms_taken #returns stthe current rooms taken
+  rooms_taken = []
+   for guest in @num_of_guests
+    rooms_taken<<guest.room_needed
+   end
+  return rooms_taken 
+end 
 
 def rooms_occupancy_type
+  rooms_at_hotel = []
   for rooms in @num_of_rooms
-    @rooms_at_hotel << rooms.beds
+    rooms_at_hotel << rooms.beds
   end
-  return @rooms_at_hotel
+  return rooms_at_hotel
+end # The function creates an array with the number of rooms. 
+
+def room_availability  #returns rooms available
+  rooms_occupancy_type.sort & the_rooms_taken.sort
 end
 
-# The above function creates an array with the number of rooms. 
-
+######################################################
+#functions for guest information
+######################################################
 def guest_roster #returns string of names of guests
   guest_roster = []
    for guest in @num_of_guests
@@ -45,7 +69,9 @@ def guest_roster #returns string of names of guests
   return guest_roster
 end 
 
-#above and below can be comn=bine to return a full roster and detail name list of guests. 
+######################################################
+#functions for hotel reports
+######################################################
 
 def total_individual_guests #returns a total of guests
   total = 0
@@ -54,7 +80,6 @@ def total_individual_guests #returns a total of guests
   end
   return total
 end
-
 
 def hotel_revenue   #cost of stay at hotel
   money_earned = 0  #tells the variable expect fixnum
@@ -66,6 +91,9 @@ def hotel_revenue   #cost of stay at hotel
   end
 end
 
+######################################################
+#functions for checking_out guests
+######################################################
 
 def remove_guest # finally deletes name from hotel. 
   for guest in @num_of_guests
@@ -77,12 +105,11 @@ def remove_guest # finally deletes name from hotel.
   return  "Sorry you haven't checked out yet, please see one of our friendly sales assitants. "
 end
 
-def checkout
-  for guest in @num_of_guests
-    if guest.name == "Steve and Anne Thompson"#needs to be gets.chomp
-      return hotel_revenue remove_guest
-    end
-  end
+def checkout #combine all features in the checkout
+hotel_revenue 
+
+remove_guest
+
 end
 
 end
